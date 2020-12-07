6a00db258a4ad1923b71c89f7ed164e8e9f80c18
chenhongxu98 <chenhongxu98@126.com>
2020-12-07 18:56:33 +0800

    添加项目文件。

---
+++ server/源.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <winsock2.h>
+#pragma comment(lib,"ws2_32.lib")
+using namespace std;
+int main() 
+{
+	WSADATA WSAData;
+	if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
+	{
+		cout << "fail to start" << endl;
+		getchar();
+		return -1;
+	}
+	else
+	{
+		cout << "success to init" << endl;
+	}
+	SOCKET sockServer = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	return 0;
+	//fread
+
+}
