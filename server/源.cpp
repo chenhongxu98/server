6d03e85cbf4eb1a09ead53fb984a8e4aa6f9d9da
chenhongxu98 <302789532@qq.com>
2020-12-09 16:01:10 +0800

    add add

--- server/源.cpp
+++ server/源.cpp
@@ -5,7 +5,13 @@
 using namespace std;
 int main() 
 {
-	WSADATA WSAData;
+	WSADATA WSAData;//socker记录
+	sockaddr_in ServerAddr;//服务器地址信息
+	{
+		ServerAddr.sin_family = AF_INET;
+		ServerAddr.sin_port = htons(12345);
+		ServerAddr.sin_addr.s_addr=inet_addr("127.0.0.1")
+	}
 	if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
 	{
 		cout << "fail to start" << endl;
