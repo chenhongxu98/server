8e4e3ffdf56655b560e7fd9b638ffc553437088f
chenhongxu98 <chenhongxu98@126.com>
2020-12-13 19:44:52 +0800

    增加丢包

--- server/源.cpp
+++ server/源.cpp
@@ -12,13 +12,13 @@ using namespace std;
 #define TIME_OUT 500	//超时500ms
 #define max8bit 256		//使用8位校验和用256取模
 int bit = 1;			//01位
-int totalsize =  255;
+int totalsize = 255;
 char recvbuff[256];//读缓冲区（数据+标识+校验+0）在字节中的顺序就是里面计算的顺序
 char ack[6] = "ACK";
 char nak[6] = "NAK";
 using namespace std;
 
-int getbit(char* recv,int size)
+int getbit(char* recv, int size)
 {
 	return recv[size - 2];
 }
@@ -42,79 +42,86 @@ int check(char* recvbuff, int size)
 	}
 	return 0;
 }
-	/*long long int a=0;
-	for (int i = 0; i < size - 1; i++)
-	{
-		a = a + recvbuff[i];
-		cout << a << endl;
-	}
-	return 0;*/
-	/*unsigned short int h = 0;//差错校验值
-	for (int i = 0; i < size - 2; i++)//取每8位校验
-	{
-		h = h + (unsigned short int)recvbuff[i];
-		if (h >= max8bit)
-		{
-			h = h % max8bit;
-			h = h + 1;
-		}
-	}
-	h = ~h;//取反
-	h = h & 0x00FF;
-	h = h + (unsigned short int)1;//加1
-	while (h >= max8bit)//检验是否有进位
+/*long long int a=0;
+for (int i = 0; i < size - 1; i++)
+{
+	a = a + recvbuff[i];
+	cout << a << endl;
+}
+return 0;*/
+/*unsigned short int h = 0;//差错校验值
+for (int i = 0; i < size - 2; i++)//取每8位校验
+{
+	h = h + (unsigned short int)recvbuff[i];
+	if (h >= max8bit)
 	{
 		h = h % max8bit;
 		h = h + 1;
 	}
-	h = h & 0x00FF;//取最低位
-	if (h == (unsigned short int)recvbuff[size - 1])
-		cout << "ok";
-	else
-		cout << "?";
+}
+h = ~h;//取反
+h = h & 0x00FF;
+h = h + (unsigned short int)1;//加1
+while (h >= max8bit)//检验是否有进位
+{
+	h = h % max8bit;
+	h = h + 1;
+}
+h = h & 0x00FF;//取最低位
+if (h == (unsigned short int)recvbuff[size - 1])
+	cout << "ok";
+else
+	cout << "?";
+return 0;
+/*signed short int h = 0;//差错校验值
+unsigned int length;
+for (int i = 0; i < size ; i++)//取每8位校验
+{
+	h = h + (unsigned short int)recvbuff[i];
+	if (h >= max8bit)
+	{
+		h = h % max8bit;
+		h = h + 1;
+	}
+}
+while (h >= max8bit)//检验是否有进位
+{
+	h = h % max8bit;
+	h = h + 1;
+}
+h = h & 0x00FF;//取最低位
+h--;
+cout << "h:" << h << endl;
+if (h == 0)
+{
 	return 0;
-	/*signed short int h = 0;//差错校验值
-	unsigned int length;
-	for (int i = 0; i < size ; i++)//取每8位校验
-	{
-		h = h + (unsigned short int)recvbuff[i];
-		if (h >= max8bit)
-		{
-			h = h % max8bit;
-			h = h + 1;
-		}
-	}
-	while (h >= max8bit)//检验是否有进位
-	{
-		h = h % max8bit;
-		h = h + 1;
-	}
-	h = h & 0x00FF;//取最低位
-	h--;
-	cout << "h:" << h << endl;
-	if (h == 0)
-	{
-		return 0;
-		
-	}
-	else
-	{
-		cout << "r1";
-		return 1;
-	}*/
 
-void addbit(char* tosend, int size,int bit)
+}
+else
+{
+	cout << "r1";
+	return 1;
+}*/
+
+void addbit(char* tosend, int size, int bit)
 {
 	tosend[size - 2] = bit;
 }
-int jiebao(char* recvbuff, int size,SOCKET sockServer, sockaddr_in ClintAddr)
+int jiebao(char* recvbuff, int size, SOCKET sockServer, sockaddr_in ClintAddr)
 {
 	int b = getbit(recvbuff, size);
-	if ((check(recvbuff, size)==0) && bit!=b)
+	if ((check(recvbuff, size) == 0) && bit != b)
 	{
 		addbit(ack, 6, b);
-		cout << "sendack" << endl;
-		sendto(sockServer, ack, 6, 0, (SOCKADDR*)&ClintAddr, sizeof(SOCKADDR));
+		if (rand() % 10 != 3)
+		{
+			cout << "sendack" << endl;
+			sendto(sockServer, ack, 6, 0, (SOCKADDR*)&ClintAddr, sizeof(SOCKADDR));
+		}
+		else
+		{
+			cout << "丢掉" << endl;
+		}
 		bit = b;
 		return 0;
 	}
@@ -127,12 +134,12 @@ int jiebao(char* recvbuff, int size,SOCKET sockServer, sockaddr_in ClintAddr)
 	}
 	else
 		return 1;
-	
+
 }
-int main() 
+int main()
 {
 	WSADATA WSAData;//socker记录
-	sockaddr_in ServerAddr,ClintAddr;//服务器地址信息
+	sockaddr_in ServerAddr, ClintAddr;//服务器地址信息
 	{
 		ServerAddr.sin_family = AF_INET;
 		ServerAddr.sin_port = htons(4567);
@@ -165,7 +172,7 @@ int main()
 	//收链接信号
 	cout << "con" << endl << endl;
 	int che = 1;
-	while (che==1)
+	while (che == 1)
 	{
 		recvfrom(sockServer, recvbuff, totalsize, 0, (SOCKADDR*)&ClintAddr, &fromlen);
 		che = jiebao(recvbuff, 6, sockServer, ClintAddr);
@@ -177,18 +184,20 @@ int main()
 		cout << (signed short int)recvbuff[i] << endl;
 	}
 	cout << "ack,收下这个包" << endl;
-//发文件
+	//发文件
 	cout << "准备接受文件" << endl;
 	ofstream fout("a.jpg", ios::binary);
 	cout << "准备接受文件" << endl;
 	while (1)
 	{
+		cout << "3";
 		recvfrom(sockServer, recvbuff, totalsize, 0, (SOCKADDR*)&ClintAddr, &fromlen);
-		//check(recvbuff, 255);
-		int b=jiebao(recvbuff, 255, sockServer, ClintAddr);
+		cout << "1";
+		int b = jiebao(recvbuff, 255, sockServer, ClintAddr);
 		if (b != 0)
 		{
 			cout << "erroe" << endl;
+			continue;
 		}
 		string s = recvbuff;
 		if (s == "bye")
@@ -199,17 +208,4 @@ int main()
 		}
 		cout << "2";
 	}
-	
-	/*while (1)
-	{
-		ofstream fout("a.jpg", ios::binary);
-		recvfrom(sockServer, recvbuff, totalsize, 0, (SOCKADDR*)&ClintAddr, &fromlen);
-		//jiebao(recvbuff, 6);
-		cout << recvbuff << endl;
-		Sleep(500);
-		sendto(sockServer, ack, 5, 0, (SOCKADDR*)&ClintAddr, sizeof(SOCKADDR));
-	}
-	return 0;*/
-	//fread
-	//怎么更改链接啊
 }
